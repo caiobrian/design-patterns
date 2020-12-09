43ab170e5164155a27160d18487743a1149fbcbe
Caio Brian <51389620+caiobrian@users.noreply.github.com>
2020-12-09 11:00:07 -0300

    Update module_a.ts

--- src/creational/singleton/module_a.ts
+++ src/creational/singleton/module_a.ts
@@ -3,6 +3,6 @@ import { DatabaseIFFE } from './db/database-iffe'
 const database = DatabaseIFFE
 database.addUser({ name: 'Caio', age: 28 })
 database.addUser({ name: 'Marisa', age: 24 })
-database.addUser({ name: 'Lalau', age: 100 })
+database.addUser({ name: 'Ash', age: 100 })
 
 export { database }
