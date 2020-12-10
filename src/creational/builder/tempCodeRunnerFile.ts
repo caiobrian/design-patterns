472cbcf16ee05f5a7754cd68fb2edd09605d7202
Caio <caiogomeslog@gmail.com>
2020-12-10 12:54:32 -0300

    remove unused code

--- src/creational/builder/tempCodeRunnerFile.ts
+++
@@ -1,9 +0,0 @@
-const mainDishBuilder = new MainDishBuilder()
-mainDishBuilder.makeMeal().makeDessert().makeBeverage()
-console.log(mainDishBuilder.getMeal())
-console.log(mainDishBuilder.getPrice())
-
-mainDishBuilder.reset()
-
-const meal2 = mainDishBuilder.makeBeverage().getMeal()
-console.log(meal2)
